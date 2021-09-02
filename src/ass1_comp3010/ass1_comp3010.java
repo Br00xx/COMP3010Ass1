7740b5a4c3b296bf5d0d709b6460be048337f6cf
br00xx <joshua_brookes3@hotmail.com>
2021-09-02 20:05:44 +1000

    Insane Speeds!!

--- src/ass1_comp3010/ass1_comp3010.java
+++ src/ass1_comp3010/ass1_comp3010.java
@@ -13,59 +13,35 @@ public class ass1_comp3010 {
 		
 		//Ask how many groups
 		System.out.println("Enter the number of groups from which you must find representatives:");
-		String input = scan.nextLine();
-		groupCount = Integer.parseInt(input);
+		groupCount = scan.nextInt();
 		
-		ArrayList<Integer> members = new ArrayList<Integer>();
-		ArrayList<Integer> memGroup = new ArrayList<Integer>();
+		// groupCount*2 as there needs to be at least one member per group followed by a 0
+		ArrayList<Integer> members = new ArrayList<Integer>(groupCount*2);
+		ArrayList<Integer> memGroup = new ArrayList<Integer>(groupCount*2);
 		
 		//Ask the members belonging to each group
 		System.out.println("Enter the list of members of each group");
 		
 		//Collecting all the members and splitting into an array
-		input = scan.nextLine();
-		for(int i=1; i<groupCount; i++) {
-			input = input + " " + scan.nextLine();
-		}
-		
-		long start = System.currentTimeMillis();
-		
-		String[] temp = input.split(" "); 
-		int[] membersTemp = new int[temp.length];
-		for(int i=0; i<temp.length; i++) {
-			membersTemp[i] = Integer.parseInt(temp[i]);
-		}
-		int[] groupTemp = new int[temp.length];
-		
-		//Making a second array of same length but the same index on second array as the first
-		// is the associated group number
-		int grp = 0;
-		for(int i=0; i<temp.length; i++) {
-			if(!temp[i].equals("0")) {
-				groupTemp[i] = grp;
-			} 
-			else {
-				groupTemp[i] = grp;
+		int grp = 1;
+		while(grp <= groupCount) {
+			Integer temp = scan.nextInt();
+			if(temp != 0) {
+				members.add(temp);
+				memGroup.add(grp);
+			} else {
 				grp++;
 			}
 		}
 		
+		long start = System.currentTimeMillis();
+		
 		//Merge sort the two list in sync.
 		// O(nlogn)
-		sort(membersTemp, groupTemp, 0, membersTemp.length-1);
+		sort(members, memGroup, 0, members.size()-1);
 		
 		//Close Scanner
 		scan.close();
-		
-		//Adding members numbers are associated group number to ArrayLists
-		// O(n)
-		for(int i=0; i<membersTemp.length; i++) {
-			if(!(membersTemp[i] == 0)) {
-				//System.out.println(membersTemp[i] + " " + groupTemp[i]);
-				members.add(membersTemp[i]);
-				memGroup.add(groupTemp[i]);
-			}
-		}
 		
 		//Run pickMembers then remove members until ArrayList is empty
 		ArrayList<Integer> selectedMember = new ArrayList<Integer>();
@@ -135,62 +111,26 @@ public class ass1_comp3010 {
 		
 	}
 	
-	//Old method
-	// Not good efficiency
-	public static String findMembers(ArrayList<String[]> members) {
-		int longestStreak = 0;
-		int currentStreak;
-		String memberID = "";
-		
-		if(members.size() == 1) {
-			return members.get(0)[0];
-		}
-		
-		for(int i=0; i<members.size()-1; i++) {
-			for(int j=0; j<members.get(i).length; j++) {
-				if(!members.get(i)[j].equals("0")) {
-					currentStreak = 1;
-					for(int k=i+1; k<members.size(); k++) {
-						for(int l=0; l<members.get(k).length; l++) {
-							if(!members.get(k)[l].equals("0") && members.get(i)[j].equals(members.get(k)[l])) {
-								currentStreak++;
-								if(currentStreak>longestStreak) {
-									longestStreak = currentStreak;
-									memberID = members.get(i)[j];
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		//System.out.println("Chosen");
-		//System.out.println(longestStreak + " " + memberID);
-		
-		return memberID;
-	}
-	
-    public static void merge(int mem[], int[] grp, int l, int m, int r)
+    public static void merge(ArrayList<Integer> mem, ArrayList<Integer> grp, int l, int m, int r)
     {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
  
         /* Create temp arrays */
-        int lMem[] = new int[n1];
-        int lGrp[] = new int[n1];
-        int rMem[] = new int[n2];
-        int rGrp[] = new int[n2];
+        Integer lMem[] = new Integer[n1];
+        Integer lGrp[] = new Integer[n1];
+        Integer rMem[] = new Integer[n2];
+        Integer rGrp[] = new Integer[n2];
  
         /*Copy data to temp arrays*/
         for (int i = 0; i < n1; ++i) {
-        	lMem[i] = mem[l + i];
-        	lGrp[i] = grp[l + i];
+        	lMem[i] = mem.get(l + i);
+        	lGrp[i] = grp.get(l + i);
         }
         for (int j = 0; j < n2; ++j) {
-        	rMem[j] = mem[m + 1 + j];
-        	rGrp[j] = grp[m + 1 + j];
+        	rMem[j] = mem.get(m + 1 + j);
+        	rGrp[j] = grp.get(m + 1 + j);
         }
             
  
@@ -203,13 +143,13 @@ public class ass1_comp3010 {
         int k = l;
         while (i < n1 && j < n2) {
             if (lMem[i] <= rMem[j]) {
-                mem[k] = lMem[i];
-                grp[k] = lGrp[i];
+            	mem.set(k, lMem[i]);
+                grp.set(k, lGrp[i]);
                 i++;
             }
             else {
-                mem[k] = rMem[j];
-                grp[k] = rGrp[j];
+                mem.set(k, rMem[j]);
+                grp.set(k, rGrp[j]);
                 j++;
             }
             k++;
@@ -217,16 +157,16 @@ public class ass1_comp3010 {
  
         /* Copy remaining elements of L[] if any */
         while (i < n1) {
-            mem[k] = lMem[i];
-            grp[k] = lGrp[i];
+            mem.set(k, lMem[i]);
+            grp.set(k, lGrp[i]);
             i++;
             k++;
         }
  
         /* Copy remaining elements of R[] if any */
         while (j < n2) {
-            mem[k] = rMem[j];
-            grp[k] = rGrp[j];
+            mem.set(k, rMem[j]);
+            grp.set(k, rGrp[j]);
             j++;
             k++;
         }
@@ -234,7 +174,7 @@ public class ass1_comp3010 {
  
     // Main function that sorts arr[l..r] using
     // merge()
-    public static void sort(int members[], int[] groups, int l, int r)
+    public static void sort(ArrayList<Integer> members, ArrayList<Integer> groups, int l, int r)
     {
         if (l < r) {
             // Find the middle point
